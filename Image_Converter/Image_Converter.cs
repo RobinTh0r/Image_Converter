d4c69a4e770f5147db5d6cab79cf2a999315de73
Robin Thor <slash.and.axl@hotmail.de>
2020-03-11 20:45:55 +0100

    V1 - Build-In PNG-only converter

---
+++ Image_Converter/Image_Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Image_Converter
+{
+    class Image_Converter
+    {
+        static void Main(string[] args)
+        {
+
+
+            string Icon_Input = @"C:\temp\test.png";
+            string Icon_Output = @"C:\temp\test.ico";
+            using (FileStream stream = System.IO.File.OpenWrite(Icon_Output))
+            {
+                Bitmap bitmap = (Bitmap)Image.FromFile(Icon_Input);
+                Icon.FromHandle(bitmap.GetHicon()).Save(stream);
+            }
+
+
+        }
+    }
+}
