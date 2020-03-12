b23cb16ab87922002f03f1a3499022163a1e01bf
Robin Thor <slash.and.axl@hotmail.de>
2020-03-12 10:24:40 +0100

    different built in method

--- Image_Converter/Image_Converter.cs
+++ Image_Converter/Image_Converter.cs
@@ -10,16 +10,34 @@ namespace Image_Converter
         {
 
 
-            string Icon_Input = Path.Combine(Environment.CurrentDirectory, @"..\..\..\media\", "test.png");
-            string Icon_Output = Path.Combine(Environment.CurrentDirectory, @"..\..\..\media\", "test.ico");
-            using (FileStream stream = System.IO.File.OpenWrite(Icon_Output))
+
+            string icon_input = Path.Combine(Environment.CurrentDirectory, @"..\..\..\media\", "test.png");
+            string icon_output = Path.Combine(Environment.CurrentDirectory, @"..\..\..\media\", "test.ico");
+
+
+
+            using (FileStream stream = System.IO.File.OpenWrite(icon_output))
             {
-                Bitmap bitmap = (Bitmap)Image.FromFile(Icon_Input);
+                Bitmap bitmap = (Bitmap)Image.FromFile(icon_input);
                 Icon.FromHandle(bitmap.GetHicon()).Save(stream);
-               Console.WriteLine("Erfolg!");
+                Console.WriteLine("Erfolg!");
             }
 
+            // Built-In Bitmap Converter --- Low Quality!
 
+            //Bitmap Cbitmap = null;
+            //{
+            //    Cbitmap = new Bitmap(icon_input); //(32, 32, PixelFormat.Format64bppPArgb);
+
+            //    Cbitmap.MakeTransparent(Color.White);
+            //    System.IntPtr icH = Cbitmap.GetHicon();
+            //    Icon ico = Icon.FromHandle(icH);
+            //    Cbitmap.Dispose();
+            //    FileStream f = new FileStream(icon_output, System.IO.FileMode.OpenOrCreate);
+            //    ico.Save(f);
+
+
+            //}
         }
     }
 }
